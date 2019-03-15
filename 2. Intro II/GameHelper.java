50070e0c8360eb2b72000fd72c9159c9069611c3
Nikos <ael4evernikos@yahoo.gr>
2019-03-15 06:03:34 +0200

    Add files via upload

---
+++ 2. Intro II/GameHelper.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class GameHelper {
+
+
+  public String getUserInput(String prompt) {
+     String inputLine = null;
+     System.out.print(prompt + "  ");
+     try {
+       BufferedReader is = new BufferedReader(
+	 new InputStreamReader(System.in));
+       inputLine = is.readLine();
+       if (inputLine.length() == 0 )  return null; 
+     } catch (IOException e) {
+       System.out.println("IOException: " + e);
+     }
+     return inputLine.toLowerCase();
+  }
+
+}
